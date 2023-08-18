47559a7077015b5244d936642f4087235c61c05c
Student <anyangojolieng@gmail.com>
2023-08-18 10:01:23 +0300

    making changes

---
+++ index.js
@@ -0,0 +1,9 @@
+
+  const menuToggle = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.menu');
+
+  menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+  });
+
